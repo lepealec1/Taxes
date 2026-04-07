69b56bcedcd407fba9124562071d96d5951268b2
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-07 01:23:21 -0700

    g

--- Server.py
+++ Server.py
@@ -25,7 +25,7 @@ credentials_info = {
 }
 
 
-TOKEN_FILE = os.path.join(BASE_DIR, "token.pkl")
+
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 FOLDER_ID = "1X7OA9TyD7cVTYXhLrj--Z_T7mQqXu5nt"  # Your Google Drive folder
 
