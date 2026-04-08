43b66df6ec3d1f76db0acaac8a47af4c974e2014
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-07 23:23:09 -0700

    a

--- Server.py
+++ Server.py
@@ -31,6 +31,7 @@ credentials_info = {
         "auth_uri": st.secrets["google_oauth"]["auth_uri"],
         "token_uri": st.secrets["google_oauth"]["token_uri"],
         "auth_provider_x509_cert_url": st.secrets["google_oauth"]["auth_provider_x509_cert_url"],
+        "redirect_uris": st.secrets["google_oauth"]["redirect_uris"],
     }
 }
 st.write(credentials_info)
