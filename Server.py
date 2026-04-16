a194e70e1dc6eba9f01e23f85a5cef2114c5f94a
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-16 00:12:04 -0700

    a

--- Server.py
+++ Server.py
@@ -167,15 +167,14 @@ st.metric("📧 Emails Sent Successfully:", st.session_state.email_count)
 
 from datetime import date
 
-pdf_file = generate_pdf(answers)
-with open(pdf_file, "rb") as f:
-    pdf_bytes = f.read()    
+
+result = generate_pdf(answers)
 
 today = date.today().strftime("%Y-%m-%d")
 
 st.download_button(
     "Download PDF",
-    data=pdf_file,
-    file_name=f"{answers.get('name', 'user')}_VITA_Questionnaire_{today}.pdf",
+    data=result["pdf_bytes"],
+    file_name=f"{result['filename'].replace('.pdf','')}_{today}.pdf",
     mime="application/pdf"
 )
